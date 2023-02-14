cfa3fa0b925fd88c6e4730da1bbccd2cf3621358
Lawal Ameh <amehlawal@gmail.com>
2023-02-14 13:23:37 +0100

    wrote another program picnicTable, made it executable, and also added the shebang line

---
+++ picnicTable.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python3
+#in this program we define a printPicnic() method that will take in a dictionary
+#and use center(),ljust() and rjust() to display the information in a neatly aligned
+# table-like format
+
+def printPicnic(itemsDict, leftWidth, rightWidth):
+    print('PICNIC ITEMS'.center(leftWidth +rightWidth, '-'))
+    for k,v in itemsDict.items():
+        print(k.ljust(leftWidth, '.') + str(v).rjust(rightWidth))
+
+picnicItems = {'sandwich':4,'apples':12,'cups':4,'cookies':8000}
+printPicnic(picnicItems,12,4)
